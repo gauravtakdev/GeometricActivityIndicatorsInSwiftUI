502cf5059ddefb4953584b9291ede0d9589414f9
Gaurav Tak <gaurav.tak@roro.io>
2025-11-15 19:20:45 +0530

    Initial COmmit

---
+++ GeometricActivityIndicators/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  GeometricActivityIndicators
+//
+//  Created by Gaurav Tak on 15/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ScrollView {
+            VStack {
+                Text("6 Geometric Custom Activity Indicators").bold().foregroundColor(.white).font(.largeTitle).padding(.vertical, 20).multilineTextAlignment(.center)
+                    .frame(alignment: .center)
+                
+                
+            }
+        }.frame(maxWidth: .infinity, maxHeight: .infinity)
+            .background(Color.black.opacity(0.9))
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ GeometricActivityIndicators/GeometricActivityIndicatorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricActivityIndicatorsApp.swift
+//  GeometricActivityIndicators
+//
+//  Created by Gaurav Tak on 15/11/25.
+//
+
+import SwiftUI
+
+@main
+struct GeometricActivityIndicatorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
